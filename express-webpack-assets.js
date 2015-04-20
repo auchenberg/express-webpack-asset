64eefb7464457ae990b96ce595a40d7a6f5722bd
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-04-20 13:33:35 +0700

    Added new devMode-option that's forcing reloading of the manifest.

--- express-webpack-assets.js
+++ express-webpack-assets.js
@@ -1,41 +1,43 @@
 var fs = require('fs');
 
-module.exports = function(manifestPath) {
+module.exports = function(manifestPath, options) {
 
-    var manifest;
-    var isManifestLoaded = false;
+    var manifest
+    var isManifestLoaded = false
+
+    options = options || {
+      devMode : false
+    }
 
     function loadManifest() {
 
-        try {
-            var data = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));    
-            isManifestLoaded = true;
-            return data;   
-        } catch(e) {
-            console.log('could not load asset manifest', e);
-        }
+      try {
+          var data = JSON.parse(fs.readFileSync(manifestPath, 'utf8'))
+          isManifestLoaded = true
+          return data
+      } catch(e) {
+          console.log('could not load asset manifest', e);
+      }
 
     }
 
     function getAsset(path) {
 
-        if(!isManifestLoaded) {
-            manifest = loadManifest();
-        }
+      if(options.devMode || !isManifestLoaded) {
+          manifest = loadManifest()
+      }
 
-        if(manifest) {
-            return manifest[path];
-        } else {
-            return path;
-        }  
+      if(manifest) {
+          return manifest[path]
+      } else {
+          return path
+      }
 
     }
 
     return function(req, res, next) {
-
-        res.locals.webpack_asset = getAsset;
-
-        next();
+      res.locals.webpack_asset = getAsset
+      next()
     };
 
 }
