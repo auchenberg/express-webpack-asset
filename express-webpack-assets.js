9e239ebfab5b8342603deba53e8de0620e1210fa
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-04-20 13:34:25 +0700

    Use JS Standard style.

--- express-webpack-assets.js
+++ express-webpack-assets.js
@@ -1,43 +1,38 @@
-var fs = require('fs');
+var fs = require('fs')
 
-module.exports = function(manifestPath, options) {
+module.exports = function (manifestPath, options) {
+  var manifest
+  var isManifestLoaded = false
 
-    var manifest
-    var isManifestLoaded = false
+  options = options || {
+    devMode: false
+  }
 
-    options = options || {
-      devMode : false
+  function loadManifest () {
+    try {
+      var data = JSON.parse(fs.readFileSync(manifestPath, 'utf8'))
+      isManifestLoaded = true
+      return data
+    } catch(e) {
+      console.log('could not load asset manifest', e)
+    }
+  }
+
+  function getAsset (path) {
+    if (options.devMode || !isManifestLoaded) {
+      manifest = loadManifest()
     }
 
-    function loadManifest() {
-
-      try {
-          var data = JSON.parse(fs.readFileSync(manifestPath, 'utf8'))
-          isManifestLoaded = true
-          return data
-      } catch(e) {
-          console.log('could not load asset manifest', e);
-      }
-
+    if (manifest) {
+      return manifest[path]
+    } else {
+      return path
     }
+  }
 
-    function getAsset(path) {
-
-      if(options.devMode || !isManifestLoaded) {
-          manifest = loadManifest()
-      }
-
-      if(manifest) {
-          return manifest[path]
-      } else {
-          return path
-      }
-
-    }
-
-    return function(req, res, next) {
-      res.locals.webpack_asset = getAsset
-      next()
-    };
+  return function (req, res, next) {
+    res.locals.webpack_asset = getAsset
+    next()
+  }
 
 }
